b83acb493686d2409cfe5a8c04588da49b151bd6
HwaJong <hwajong1016@gmail.com>
2023-01-24 11:54:20 +0900

    project_setting

---
+++ src/test/java/ghkwhd/upload/UploadApplicationTests.java
@@ -0,0 +1,13 @@
+package ghkwhd.upload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
